b36bb81518e1f4fc7be8875f7b82d820fe51fe90
Icybearfvreal <arcader4g@gmail.com>
2024-07-31 23:13:37 +0100

    Next.jsL1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
+import homeImg from "../../public/home.jpg";
+import Hero from "@/components/hero";
 
 export default function Home() {
-  return <div>
-    <div>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
-    </div>
-    Home Page
-    </div>;
+  return  (
+    <Hero 
+    imgData={homeImg} 
+    imgAlt="car factory"
+    title="Professional Cloud Hosting"
+    />
+  );
 }
